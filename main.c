8f8f9b89532eb254a249f65329836b91f18f660a
IanLewis42 <IanLewis42@users.noreply.github.com>
2017-08-24 12:20:38 +0100

    Move some main() code into findchain() function, plus misc changes.

--- main.c
+++ main.c
@@ -20,14 +20,15 @@ char word[10];
 wordtype words[10];
 wordtype *current_word, *last_word;
 
-int isindict(char* word, int length);
+int findchain(void);
 wordtype* findword(wordtype* current_word, int pos);
 wordtype* findword2(wordtype* current_word, int pos);
+int isindict(char* word, int length);
 
 int main(int argc, char* argv[])
 {
     char dict_word[50],name[20];
-    int count=0,chosen,tries=0,changed,pos;
+    int count=0,chosen,tries=0;//,changed,pos;
 
     int i,j;
 
@@ -35,16 +36,13 @@ int main(int argc, char* argv[])
 
     srand(time(0));
 
-    //get word
-    //word_length = 5;
-    //strncpy(word,"sport",word_length+1);
+    //printf("argc = %d",argc);
 
-    printf("argc = %d",argc);
-
-    if (argc == 2)
+    if (argc == 2)  //word supplied on cmdline
     {
         strncpy(word, (char*)argv[1], strlen((char*)argv[1])+1);    //copy cmdline arg to word. +1 for null terminator.
         word_length = strlen(word);
+        chain_length = word_length;
         //printf("length = %d",word_length);
         //printf("word = %s",word);
 
@@ -53,10 +51,27 @@ int main(int argc, char* argv[])
     }
     else
     {
-        word_length = 4+rand()%3;
-        //printf("length = %d",word_length);
+        if (argc == 3) //wl/cl on cmdline
+        {
+            word_length = atoi(argv[1]);
+            chain_length = atoi(argv[2]);
+            printf("WL:%d CL:%d\n",word_length,chain_length);
+        }
+
+        else
+        {
+            word_length = 4+rand()%3;
+            chain_length = word_length;
+            //printf("length = %d",word_length);
+        }
         sprintf(name,"dict%d.txt",word_length);
         dict = fopen(name,"r");
+        if (dict == NULL)
+        {
+            printf("Failed to open dictionary file:%s\n",name);
+            return 0;
+        }
+
         while (fgets(dict_word,50,dict) != NULL) count++;        //count words
         rewind(dict);
     }
@@ -66,13 +81,13 @@ int main(int argc, char* argv[])
         if (argc != 2)
         {
             chosen = rand()%count;                              //pick random word
-            printf("count = %d, chosen = %d\n",count,chosen);
+            //printf("count = %d, chosen = %d\n",count,chosen);
             rewind(dict);
             for (i=0 ; i<chosen ; i++)
                 fgets(word,50,dict);
 
             word[strcspn(word, "\n")] = 0;      //trim cr/lf
-            printf("word = %s\n",word);
+            //printf("word = %s\n",word);
             rewind(dict);
         }
 
@@ -88,6 +103,10 @@ int main(int argc, char* argv[])
                      //printf("%s",dict_word);
                      break;
                 }
+                if (dict_word[0] > i + 'a') //we've 'skipped ahead, meaning there are no words starting with this letter
+                {
+                    break;                  //break anyway; we'll never have a word starting with this letter in a chain,
+                }                           // if user puts one in, we won't find it regardless.
             }
         }
 
@@ -95,68 +114,103 @@ int main(int argc, char* argv[])
 
         current_word = &words[0];
         strncpy(current_word->word, word,word_length+1);          //put first word into array
+/*
+        if (searching)
+        {
+            tries++;
+            if (tries == 100)
+            {
+                //new word
+            }
+            else
+            {
+                if (findchain())
+                    searching = FALSE;
+            }
+        }
+        if (event)
+        {
+            //handle touches
 
-        tries = 0;
+            //redraw on timer - draw fn handles start, menu, game screens?
+
+            //when level finished, searching = true.
+        }
+*/
 
         //come around here each time we've decided we can't make a chain from the chosen starting word
-        while(!success)
+        for (tries=0 ; tries<100 ; tries++)
         {
-            //printf("X");
-            tries++;
-            if (tries == 100) break;    //arbitrary!
-
-            current_word = &words[0];
-            strncpy(current_word->word, word,word_length+1);        //put first word into array
-
-            //reset variables
-            fail = 0;                                               //successive failures to find the next word
-            tried_map = 0;                                          //bitmap of positions we've tried
-            change_map = 0;                                         //bitmap of which letters have been changed
-
-            //testing
-            //chain_length = 6;
-
-            chain_length = word_length;
-            if (chain_length > word_length) easy = FALSE;       //otherwise we lock up....
-                                                                //in this case, need to check new word against all old words....
-
-            //change as many letters as the chain length
-            for (changed=0 ; changed<chain_length ; )//i++)
+            if (findchain())
             {
-                //printf(".");
-                while(1)        //pick random position to change - don't duplicate
-                {
-                    pos = rand() % word_length;
-                    if ( ((change_map & (1<<pos)) == 0) && ((tried_map & (1<<pos)) == 0 ) ) break;
-                }
-
-                tried_map |= (1<<pos);          //remember we tried this position
-
-                last_word = current_word;
-                current_word = findword2(current_word, pos);    //findword(2) takes and returns returns pointer into word list.
-                                                                //If unchanged, no new word found
-                if (current_word != last_word)
-                {
-                    fail=0;                                     //new word, so reset fail count
-                    tried_map = 0;                                  //and tried map
-                    changed++;
-                }
-                else
-                    fail++;
-
-                if (changed==chain_length) success = TRUE;      //changed every letter, so done!
-
-                if (fail == (word_length - changed))            //consecutive failures == unchanged positions. So give up.
-                    break;                                      //(just word_length if easy == FALSE)
-
+                success = TRUE;
+                break;
             }
         }
     }
 
-    for (j=0 ; j<=changed ; j++)
+    for (j=0 ; j<=chain_length ; j++)
         printf("\n%s",words[j].word);
 
     fclose(dict);
+    return 0;
+}
+
+int findchain(void)
+{
+    int changed,pos;
+
+    current_word = &words[0];
+    strncpy(current_word->word, word,word_length+1);        //put first word into array
+
+    //reset variables
+    fail = 0;                                               //successive failures to find the next word
+    tried_map = 0;                                          //bitmap of positions we've tried
+    change_map = 0;                                         //bitmap of which letters have been changed
+
+    //chain_length = word_length;
+    //chain_length = 6;               //testing
+
+    if (chain_length > word_length) easy = FALSE;       //otherwise we lock up....
+                                                        //in this case, need to check new word against all old words....
+
+    //change as many letters as the chain length
+    for (changed=0 ; changed<chain_length ; )//i++)
+    {
+        //printf(".");
+        while(1)        //pick random position to change - don't duplicate
+        {
+            pos = rand() % word_length;
+            if ( ((change_map & (1<<pos)) == 0) && ((tried_map & (1<<pos)) == 0 ) ) break;
+        }
+
+        tried_map |= (1<<pos);          //remember we tried this position
+
+        last_word = current_word;
+        current_word = findword2(current_word, pos);    //findword(2) takes and returns returns pointer into word list.
+                                                        //If unchanged, no new word found
+        if (current_word != last_word)
+        {
+            fail=0;                                     //new word, so reset fail count
+            tried_map = 0;                                  //and tried map
+            changed++;
+        }
+        else
+            fail++;
+
+        if (changed==chain_length)  return 1;      //changed every letter, so done!
+
+        if (easy)
+        {
+            if (fail == (word_length - changed))            //consecutive failures == unchanged positions. So give up.
+                break;
+        }
+        else
+        {
+            if (fail == (word_length))                      //consecutive failures == positions. So give up.
+                break;
+        }
+    }
     return 0;
 }
 
@@ -171,12 +225,9 @@ Not sure if this is more efficient or not.......
 wordtype* findword2(wordtype* current_word, int pos)
 {
     wordtype matches[100];    //matches
+    wordtype* wordptr;
     int i,j=0,fail,chosen;//,i;
     char dict_word[50];
-
-    //pos=0;
-
-    //printf("Start FW2:%s,%d\n",current_word->word,pos);
 
     rewind(dict);
 
@@ -184,10 +235,6 @@ wordtype* findword2(wordtype* current_word, int pos)
     {
         //check for only difference in position 'pos'
         fail=0;
-
-        //printf("list:%s dict:%s\n",current_word->word,dict_word);
-        //if (getch() == 'q') exit(0);
-
         for (i=0 ; i<word_length ; i++)
         {
             if (i==pos)
@@ -195,7 +242,6 @@ wordtype* findword2(wordtype* current_word, int pos)
                 if (dict_word[i] == current_word->word[i])
                 {
                     fail = 1;
-                    //printf("== fail at %d\n",i);
                     break;
                 }
             }
@@ -203,59 +249,41 @@ wordtype* findword2(wordtype* current_word, int pos)
             {
                 if (dict_word[i] != current_word->word[i])
                 {
-
-                    //printf("!= fail at %d\n",i);
                     fail = 1;
                     break;
                 }
             }
-
         }
         if (fail == 0)
         {
-            strncpy(matches[j].word,dict_word,word_length);
-            matches[j].word[word_length]=0;
-            //printf("%s\n",matches[j].word);
-            j++;
-            //if (getch() == 'q') exit(0);
+            //check here for duplication??
+            for (wordptr=&words[0] ; wordptr<current_word ; wordptr++)
+            {
+                if (strncmp(dict_word,wordptr->word,word_length)==0)
+                    fail = 1;
+            }
+            if (fail == 0)
+            {
+                strncpy(matches[j].word,dict_word,word_length);
+                matches[j].word[word_length]=0;
+                j++;
+            }
         }
     }
 
-    if (j==0)
-    {
-        //printf("End of dictionary, no matches\n");
-        return current_word; //fail
-    }
+    if (j==0) return current_word; //fail
 
-    chosen = rand()%j;
-    //printf("End of dictionary, chosen %d of %d matches (%s)\n",chosen,j,matches[chosen].word);
-    //if (getch() == 'q') exit(0);
-
-    /*for (pos=0 ; pos<word_length ; pos++)
-    {
-        if (dict_word[pos] != matches[chosen].word[pos])
-        break;
-    }*/
-
-    //printf("pos = %d\n",pos);
+    chosen = rand()%j;                              //pick one of our matches at randonm
     if (easy)
         change_map |= (1<<pos);                    //remember we changed this position
     else
     {
-        //check against rest of chain
+
     }
-    //found = TRUE;                               //remember we've got a match
-    //strncpy(word,matches[chosen].word,word_length+1);           //copy it back for next attempt
 
-    //fail = 0;                                   //reset fail counter
-    tried_map = 0;                                  //reset which positions we tried
-    //changed++;                                        //inc replaced letter count
-    //strncpy(words[changed].word, word,word_length+1); //remember next word in sequence
-    current_word++;
-    strncpy(current_word->word,matches[chosen].word,word_length+1); //remember next word in sequence
-    //if (changed==chain_length) success = TRUE;        //changed every letter, so done!
-
-    //printf("Stop FW2:%s,%d\n",current_word->word,pos);
+    tried_map = 0;                                                  //reset which positions we tried
+    current_word++;                                                 //inc pointer
+    strncpy(current_word->word,matches[chosen].word,word_length+1); //copy this word to next word in sequence
 
     return(current_word);
 }
